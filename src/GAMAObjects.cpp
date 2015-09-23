ac771dda283f1734700d53dc5a80fce6c8b701de
Reiko Nakajima <reiko.nakajima@gmail.com>
2015-09-23 15:46:08 +0200

    allow FITS file input of GAMA objects (instead of Edo's ASCII files) (#3)

--- src/GAMAObjects.cpp
+++ src/GAMAObjects.cpp
@@ -3,6 +3,7 @@
 //
 #include "GAMAObjects.h"
 #include "StringStuff.h"
+#include <algorithm>  // std::random_shuffle
 using namespace std;
 
 
@@ -30,11 +31,11 @@ GAMAObject::printLine(ostream& os) const{
      << setprecision(1) << setw(10)
      << z << " "
      << absmag_r << " "
-     << d_absmag_r << " "
+    //<< d_absmag_r << " "
      << logmstar << " "
-     << d_logmstar << " "
+    //<< d_logmstar << " "
      << uminusr << " "
-     << d_uminusr << " "
+    //<< d_uminusr << " "
      << endl;
   return;
 }
@@ -49,14 +50,95 @@ GAMAObjectList::GAMAObjectList(istream& is) {
 }
 
 
+GAMAObjectList::GAMAObjectList(const string fits_filename, int max_count) {
+
+  // open FITS file
+  const string obj_extension = "JOINED";
+  auto_ptr<CCfits::FITS> pInfile(0);
+
+  try {
+    // open the fits table and go to the right extension
+    pInfile.reset(new CCfits::FITS(fits_filename, CCfits::Read, obj_extension));
+
+  } catch (CCfits::FITS::CantOpen &fitsError) {
+      throw GAMAObjectsError(string("Error opening the fits file with message: ") +
+			     fitsError.message());
+  } catch (CCfits::FITS::NoSuchHDU &fitsError) {
+      throw GAMAObjectsError(string("Error going to the HDU: ") + fitsError.message());
+  } catch (CCfits::FitsException &fitsError) {
+      throw GAMAObjectsError(string("Error in FITS: ") + fitsError.message());
+  }
+
+  // goto OBJECTS extension
+  CCfits::ExtHDU& table = pInfile->extension(obj_extension);
+
+  // read the following columns (annoyingly, only one column can be read at a time):
+  //  id, RA/DEC, Z_1, FIELD_POS, R_COMOVING
+
+  valarray<double> ra;
+  CCfits::Column& column1 = table.column("RA");
+  column1.read( ra, 1, column1.rows() );
+  valarray<double> dec;
+  CCfits::Column& column2 = table.column("DEC");
+  column2.read( dec, 1, column2.rows() );
+
+  valarray<float> z;
+  CCfits::Column& column3 = table.column("Z_1");
+  column3.read( z, 1, column3.rows() );
+
+  valarray<float> fluxscale;
+  CCfits::Column& column4 = table.column("fluxscale");
+  column4.read( fluxscale, 1, column4.rows() );
+
+  valarray<float> logmstar;
+  CCfits::Column& column5 = table.column("logmstar");
+  column5.read( logmstar, 1, column5.rows() );
+
+  valarray<float> absmag_r;
+  CCfits::Column& column6 = table.column("absmag_r");
+  column6.read( absmag_r, 1, column6.rows() );
+
+  valarray<float> uminusr;
+  CCfits::Column& column7 = table.column("uminusr");
+  column7.read( uminusr, 1, column7.rows() );
+
+  valarray<float> r_comoving;
+  CCfits::Column& column8 = table.column("R_COMOVING");
+  column8.read( r_comoving, 1, column8.rows() );
+
+
+  // create array of indices 0 through (num_randoms-1)
+  int num_randoms = column1.rows();
+  int index[num_randoms];
+  for (int i=0; i<num_randoms; ++i) {
+    index[i] = i;
+  }
+  // randomize the index array, then truncate at max_count
+  if (max_count <= 0) {
+    max_count = num_randoms;
+  }
+  random_shuffle(&index[0], &index[num_randoms]);
+  lens_list.reserve(max_count);
+  for (long int i=0; i<max_count; ++i) {
+    int ii = index[i];  // randomized index
+    GAMAObject* ptr = new GAMAObject(ii, ra[ii], dec[ii], z[ii], r_comoving[ii],
+				     logmstar[ii], absmag_r[ii], uminusr[ii]);
+    lens_list.push_back(ptr);
+  }
+
+  return;
+}
+
+
 int
 GAMAObjectList::applyLogMStarCut(float min_logmstar, float max_logmstar) {
   for (vector<GAMAObject*>::iterator it = lens_list.begin();
        it != lens_list.end(); /* no increment */) {
+    float logmstar = (*it)->getLogMStar();
     if ( (*it)->getLogMStar() < min_logmstar or (*it)->getLogMStar() >= max_logmstar ) {
       it = lens_list.erase(it);
     } else {
-      ++it;
+      ++it;  // increment here; only if there were no deletion
     }
   }
   return lens_list.size();
--- src/GAMAObjects.h
+++ src/GAMAObjects.h
@@ -4,6 +4,7 @@
 #ifndef GAMAOBJECTS_H
 #define GAMAOBJECTS_H
 #include "LensObjects.h"
+#include <CCfits/CCfits>
 
 class GAMAObjectsError : public MyException {
  public:
@@ -18,7 +19,7 @@ class GAMAObjectsError : public MyException {
  * This class reads the GAMA data file with galaxy positions as well as halo center (which may be
  * offset from the star position), and sets it up as a "lens" in galaxy-galaxy lensing format.
  *
- * The columns are formatted:
+ * The (Edo-generated) ASCII columns are formatted:
  *   0 -  4 CATAID, GROUPIDA, RA, DEC, Z,
  *   5 -  9 ABSMAG_R, DELABSMAG_R, LOGMSTAR, DELLOGMSTAR, UMINUSR,
  *  10 - 14 DELUMINUSR, RPETRO, LOGMOVERL, DELLOGMOVERL, LOGLWAGE,
@@ -26,22 +27,52 @@ class GAMAObjectsError : public MyException {
  *  20 - 24 LOGMREMNANTS, DELLOGMREMNATS, RANKBCG, NFOF, ZMAX_19P8,
  *  25      ZMAX_19P4
  *
+ * The FITS table formats have various columns, including
+    name = 'CATAID_1'; format = 'J'
+    name = 'RA'; format = 'D'; unit = 'deg'
+    name = 'DEC'; format = 'D'; unit = 'deg'
+    name = 'Z_1'; format = 'E'
+    name = 'logmstar'; format = 'E'; unit = 'dex'
+    name = 'uminusr'; format = 'E'; unit = 'mag'
+    name = 'absmag_r'; format = 'E'; unit = 'mag'
+    name = 'R_COMOVING'; format = '1E'; unit = 'Mpc(Planck14)'
+ *
+ *
+ *  Might be nice to retain the following quantities from the original:
+ *
+    name = 'Rpetro'; format = 'D'; unit = 'mag'
+    name = 'RankBCG'; format = 'I'
+    name = 'SepBCG'; format = 'D'; unit = 'arcsec'
+    name = 'CoSepBCG'; format = 'D'; unit = 'Mpc/h'
+    name = 'AngSepBCG'; format = 'D'; unit = 'Mpc/h'
+    name = 'RankCen'; format = 'I'
+    name = 'SepCen'; format = 'D'; unit = 'arcsec'
+    name = 'CoSepCen'; format = 'D'; unit = 'Mpc/h'
+    name = 'AngSepCen'; format = 'D'; unit = 'Mpc/h'
+ *
+ *
+ *
  */
 
 class GAMAObject : public LensObject {
 
  public:
   GAMAObject(const string buffer);
+  GAMAObject(long int _id, double _ra, double _dec, float _z, float _r_comoving,
+	     float _logmstar, float _absmag_r, float _uminusr) :
+             LensObject(_id, _ra, _dec, _z, -99), r_comoving(_r_comoving), logmstar(_logmstar),
+	     absmag_r(_absmag_r), uminusr(_uminusr) {}
   float getAbsMagR() const { return absmag_r; }
-  float getAbsMagRErr() const { return d_absmag_r; }
+  //float getAbsMagRErr() const { return d_absmag_r; }
   float getLogMStar() const { return logmstar; }
-  float getLogMStarErr() const { return d_logmstar; }
-  float getRankBCG() const { return rankbcg; }
-  int   getNfof() const { return Nfof; }
+  //float getLogMStarErr() const { return d_logmstar; }
+  //float getRankBCG() const { return rankbcg; }
+  //int   getNfof() const { return Nfof; }
   void printLine(ostream& os) const;
 
  private:
   int   group_id;
+  float r_comoving;
   float absmag_r, d_absmag_r;
   float logmstar, d_logmstar;
   float uminusr, d_uminusr;
@@ -61,6 +92,9 @@ class GAMAObjectList : public LensObjectList<GAMAObject*> {
 
  public:
   GAMAObjectList(istream& is);
+  // Constructor:  If max_count <= 0, use the entire input.
+  //               Otherwise, randomize input rows and return the first max_count objects
+  GAMAObjectList(const string fits_filename, int max_count=-1);
   int applyLogMStarCut(float min_logmstar, float max_logmstar);
 
  private:
